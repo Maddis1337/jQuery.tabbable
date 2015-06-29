e6ffaf6d91908fd5e3184292aa5faca47fae2028
Mathis Neumann <mathis@simpletechs.net>
2015-06-29 12:22:07 +0200

    avoid error if no active element exists

--- jquery.tabbable.js
+++ jquery.tabbable.js
@@ -51,9 +51,11 @@
 		var selectables = $(selector);
 		var current = $(document.activeElement);
 		var nextIndex = 0;
-		var currentIndex = selectables.index(current);
-		if(currentIndex + 1 < selectables.length){
-			nextIndex = currentIndex + 1;
+		if(current.length === 1){
+			var currentIndex = selectables.index(current);
+			if(currentIndex + 1 < selectables.length){
+				nextIndex = currentIndex + 1;
+			}
 		}
 
 		selectables.eq(nextIndex).focus();
@@ -63,10 +65,11 @@
 		var selectables = $(selector);
 		var current = $(document.activeElement);
 		var prevIndex = selectables.length - 1;
-		
-		var currentIndex = selectables.index(current);
-		if(currentIndex > 0){
-			prevIndex = currentIndex - 1;
+		if(current.length === 1){
+			var currentIndex = selectables.index(current);
+			if(currentIndex > 0){
+				prevIndex = currentIndex - 1;
+			}
 		}
 
 		selectables.eq(prevIndex).focus();
